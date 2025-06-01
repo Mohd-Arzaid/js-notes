320a159d5330f91724734e700383e8c31f9e1aad
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-06-02 01:47:24 +0530

    fix

--- src/components/manual/VariablesAndDataTypes.jsx
+++ src/components/manual/VariablesAndDataTypes.jsx
@@ -17,18 +17,18 @@ const VariablesAndDataTypes = () => {
   return (
     <div className=" max-w-[88rem] mx-auto px-4 py-8 md:p-12">
       {/* Heading */}
-      {/* <h2 className="text-[20px] md:text-[48px] text-center font-geist font-semibold text-primary">
+      <h2 className="text-[20px] md:text-[48px] text-center font-geist font-semibold text-primary">
         Variables And DataTypes Q/A
       </h2>
-      <p className="text-secondary text-center text-[15px] md:text-[20px] font-geist">
+     {/* <p className="text-secondary text-center text-[14px] md:text-[20px] font-geist">
         Variables And DataTypes{" "}
-        <span className="text-primary font-geist text-[15px] md:text-[20px] font-medium underline underline-offset-4">
+        <span className="text-primary font-geist text-[14px] md:text-[20px] font-medium underline underline-offset-4">
           Questions & Answers
         </span>
       </p> */}
 
       {/* Accordion of Q/A */}
-      <div className="w-full max-w-[1104px] mt-[8px] md:mt-[24px] mx-auto">
+      <div className="w-full max-w-[1104px]  md:mt-[24px] mx-auto">
         <Accordion type="single" collapsible className="w-full">
           {/* Q - 1 */}
           <AccordionItem value="item-1">
