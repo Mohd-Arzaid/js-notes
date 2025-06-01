98fcd19fe40a1e369831eaf3b9a061aa54b84f54
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-06-01 14:55:09 +0530

    fix

--- src/components/manual/VariablesAndDataTypes.jsx
+++ src/components/manual/VariablesAndDataTypes.jsx
@@ -26,7 +26,7 @@ const VariablesAndDataTypes = () => {
           {/* Q - 1 */}
           <AccordionItem value="item-1">
             <AccordionTrigger className="font-geist text-[15px] md:text-[18px] tracking-wider text-primary font-medium">
-              Variables ?
+            How do Computers Work?
             </AccordionTrigger>
             <AccordionContent className="font-geist text-sm md:text-[18px] tracking-wider text-secondary">
               We store data in memory and access it using its address. But since
